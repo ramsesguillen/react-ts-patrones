3f15a9994714755e7e02b67234e7ecbfabeb62fa
ramsesguillen <faraonramses97@outlook.com>
2022-02-01 21:27:15 -0600

    Component compound ready

---
+++ src/02-component-patterns/hooks/useProduct.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+
+export const useProduct = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseBy = ( val: number ) => {
+    setCounter( prev => Math.max(prev + val, 0));
+  }
+
+  return {
+    counter, increaseBy
+  }
+}
+
